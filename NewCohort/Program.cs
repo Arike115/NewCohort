5dd6cf53b6092023009291dc332bc2fa86821d2b
Arike115 <ajibolaoluwafunke669@gmail.com>
2024-07-06 12:21:34 +0100

    Add project files.

---
+++ NewCohort/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("This class is strictly C# base");
+MyMethod();
+
+//datatypes
+//string
+//interger int
+//decimal
+//float
+//bool 
+//double
+//char
+//long
+//byte
+
+string word = "chalk";
+Console.WriteLine(word);
+
+int v = 53645399;
+
+double x = 23;
+
+decimal y = 2145231423656235.435m;
+float number = 663286217487234.90f;
+bool check = false;
+
+char n = 'a';
+byte s = 35;
+long r = 7828782362778732;
+
+
+//array
+string[] result = { "pen", "biro", "chalk", "duster", "book" };
+
+foreach (string abiola in result)
+{
+    Console.WriteLine(abiola);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+static void MyMethod()
+{
+    Console.WriteLine("weclome to C# class, learn new syntax");
+   /* Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");*/
+}
+
