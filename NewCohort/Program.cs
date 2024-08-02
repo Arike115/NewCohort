0155a89ceb08776f025c3926b37b7615e37fba4e
Arike115 <ajibolaoluwafunke669@gmail.com>
2024-08-02 17:52:39 +0100

    changes

--- NewCohort/Program.cs
+++ NewCohort/Program.cs
@@ -1,53 +1,8 @@
-﻿//icollection
-//ilist
-//ienumerable
-//list
+﻿//abstraction
+//base class
 
-List<string> Wears = new List<string>();
-Wears.Add("Skirt");
-Wears.Add("Trouser");
-Wears.Add("Short");
-Wears.Add("Shirt");
-Wears.Add("Singlet");
-Wears.Add("Blouse");
+using NewCohort;
 
-collectionDisplay(Wears);
-IlistDisplay(Wears);
-ListDisplay(Wears);
-IenumerableDisplay(Wears);
-
-static void collectionDisplay(ICollection<string> mylist)
-{
-     
-    mylist.Add("Jumpsuite");
-    foreach (var item in mylist)
-        Console.WriteLine("icollection display" + " " +item);
-    Console.WriteLine("*************************");
-}
-
-static void IlistDisplay(IList<string> mylist)
-{
-    mylist.Insert(3, "joggers");
-    mylist.Add("cargo pant");
-    foreach (var item in mylist)
-        Console.WriteLine("ilistdiplay display" + " " + item);
-    Console.WriteLine("*************************");
-}
-
-static void ListDisplay(List<string> mylist)
-{
-
-    mylist.Insert(2, "polo");
-    mylist.Add("jeans");
-    mylist.Sort();
-    
-    foreach (var item in mylist)
-        Console.WriteLine("listdiplay display" + " " + item);
-    Console.WriteLine("*************************");
-}
-
-static void IenumerableDisplay(IEnumerable<string> mylist)
-{
-    var data = mylist.First();
-    Console.WriteLine("ienumerable" + " " + data);
-}
+ Triangle triangle = new Triangle();
+triangle.AreaOfAcircle();
+triangle.Radius();
---
+++ NewCohort/Triangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewCohort
+{
+    //customise
+    //obsolete
+    //web method
+    //serialization
+    //nonserializtion
+
+    public class Triangle : Circle
+    {
+        public override void AreaOfAcircle()
+        {
+            Console.WriteLine("this area is triangle not circle");
+        }
+
+        [Obsolete]
+        public void Breadth()
+        {
+
+        }
+
+        public void Breadth(int i, int y, int v)
+        {
+
+        }
+
+        public void Breadth(int x)
+        { 
+        }
+    }
+}
